dbd95bd15f42573d37525e3a596667accbb6d850
Kevin Gut <kevin.gut@bitm.sg>
2019-10-18 23:28:39 +0200

    Support compressed HTTP

---
+++ AnyBlock/DecompressClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace AnyBlock
+{
+    /// <summary>
+    /// WebClient with GZip and Deflate support
+    /// </summary>
+    public class DecompressClient : WebClient
+    {
+        private static DecompressionMethods _all = DecompressionMethods.None;
+
+        private static DecompressionMethods AllMethods
+        {
+            get
+            {
+                if (_all == DecompressionMethods.None)
+                {
+                    _all = (DecompressionMethods)Enum.GetValues(typeof(DecompressionMethods))
+                        .OfType<DecompressionMethods>()
+                        .Cast<int>()
+                        .Sum();
+                }
+                return _all;
+            }
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+
+            HttpWebRequest request = (HttpWebRequest)base.GetWebRequest(address);
+            request.AutomaticDecompression = AllMethods;
+            return request;
+        }
+    }
+}
