704105796cf48e3ee1991c110cbeba53984e53ca
Kevin Gut <kevin.gut@bitm.sg>
2019-10-18 23:58:53 +0200

    Put CIDR class into its own file

--- AnyBlock/Firewall.cs
+++ AnyBlock/Firewall.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Reflection;
 
 namespace WinAPI.NET
@@ -206,70 +205,4 @@ namespace WinAPI.NET
         public Direction Direction;
     }
 
-    public class CIDR
-    {
-        /// <summary>
-        /// Gets the IP Address
-        /// </summary>
-        public IPAddress Address
-        { get; private set; }
-        /// <summary>
-        /// Gets the Network Mask
-        /// </summary>
-        public int Mask
-        { get; private set; }
-
-        /// <summary>
-        /// Initializes a new Range in CIDR Notation
-        /// </summary>
-        /// <param name="CombinedNotation">IP/Mask</param>
-        public CIDR(string CombinedNotation)
-        {
-            IPAddress TempAddr;
-            int TempMask = 0;
-            if (string.IsNullOrWhiteSpace(CombinedNotation))
-            {
-                throw new ArgumentNullException(nameof(CombinedNotation));
-            }
-            var Parts = CombinedNotation.Split('/');
-            if (Parts.Length > 2)
-            {
-                throw new FormatException("CombinedNotation is in an invalid Format");
-            }
-            if (!IPAddress.TryParse(Parts[0], out TempAddr))
-            {
-                throw new FormatException("CombinedNotation doesn't has a valid IP Address");
-            }
-            if (Parts.Length == 1)
-            {
-                Mask = TempAddr.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6 ? 128 : 32;
-            }
-            else
-            {
-                if (!int.TryParse(Parts[1], out TempMask))
-                {
-                    throw new FormatException("CombinedNotation doesn't has a valid CIDR Mask");
-                }
-                if (TempMask < 0)
-                {
-                    throw new FormatException("CIDR Mask is outside of Bounds");
-                }
-                if (TempMask > (TempAddr.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6 ? 128 : 32))
-                {
-                    throw new FormatException("CIDR Mask is outside of Bounds");
-                }
-            }
-            Mask = TempMask;
-            Address = TempAddr;
-        }
-
-        /// <summary>
-        /// Gets this object as CIDR notation
-        /// </summary>
-        /// <returns>IP/Mask</returns>
-        public override string ToString()
-        {
-            return $"{Address}/{Mask}";
-        }
-    }
 }
